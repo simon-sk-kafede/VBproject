3f33952646315901b48f247024c0bc6b420ae507
“simon-sk-kafede” <simonks250@gmail.com>
2022-09-10 11:50:06 +0300

    My first commit

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		//
+		/public static void main(string args[])
+		{
+			console.WriteLine("My name");
+		}
+		//
+	}
+}
